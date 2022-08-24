3c7c93b9e389632d26e0e7e36253df9a60977c1c
shubhamgohad221997 <101570475+shubhamgohad221997@users.noreply.github.com>
2022-08-24 23:30:22 +0530

    Day_1

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar Page</h1>
+      <Box className='na1'>
+      </Box>
+    </div>
+  )
+}
+
+export default Navbar
